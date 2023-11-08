3c71a64cdf2bba5ab639480d2e7c30c6cc4f4706
vishant007 <vishant9797@gmail.com>
2023-11-08 18:30:45 +0530

    updated the feature where video having audio plays

---
+++ src/components/HasvideoGotAudio.js
@@ -0,0 +1,33 @@
+function hasAudio(video) {
+	return (
+		video.mozHasAudio ||
+		Boolean(video.webkitAudioDecodedByteCount) ||
+		Boolean(video.audioTracks?.length)
+	);
+}
+function hasVideoGotAudio(src) {
+	const video = document.createElement('video');
+	video.muted = true;
+	video.crossOrigin = 'anonymous';
+	video.preload = 'auto';
+
+	return new Promise((resolve, reject) => {
+		video.addEventListener('error', reject);
+
+		video.addEventListener(
+			'canplay',
+			() => {
+				video.currentTime = 0.99;
+			},
+			{ once: true } // Important because 'canplay' can be fired hundreds of times.
+		);
+
+		video.addEventListener('seeked', () => resolve(hasAudio(video)), {
+			once: true,
+		});
+
+		video.src = src;
+	});
+}
+
+export default hasVideoGotAudio;
--- src/components/VPlayer.jsx
+++ src/components/VPlayer.jsx
@@ -1,43 +1,49 @@
 import React, { useState, useRef } from 'react';
-
 import '../VPlayer.css'; // Import the CSS file
+import HasvideoGotaudio from './HasvideoGotAudio'; // Import the HasvideoGotaudio function
 
 const VPlayer = () => {
 	const videoRef = useRef();
-
 	const canvasRef = useRef();
-
 	const [isPlaying, setIsPlaying] = useState(false);
-
 	const [videoMetadata, setVideoMetadata] = useState({
 		duration: 0,
-
 		width: 0,
-
 		height: 0,
 	});
+	const [hasAudio, setHasAudio] = useState(null); // To track whether the video has audio
 
-	const loadVideo = (event) => {
+	const loadVideo = async (event) => {
 		const file = event.target.files[0];
 
 		if (file) {
 			const fileURL = URL.createObjectURL(file);
 
-			videoRef.current.src = fileURL;
-
 			// Check if the video has audio tracks
+			try {
+				const hasAudio = await HasvideoGotaudio(fileURL);
+				setHasAudio(hasAudio);
 
-			// Update video metadata (duration, width, height)
+				if (!hasAudio) {
+					// If no audio, reset video source to prevent rendering it on the canvas
+					videoRef.current.src = '';
+				} else {
+					// Set the video source only if it has audio
+					videoRef.current.src = fileURL;
 
-			videoRef.current.onloadedmetadata = () => {
-				const duration = videoRef.current.duration;
-
-				const width = videoRef.current.videoWidth;
-
-				const height = videoRef.current.videoHeight;
-
-				setVideoMetadata({ duration, width, height });
-			};
+					// Update video metadata (duration, width, height)
+					videoRef.current.onloadedmetadata = () => {
+						const duration = videoRef.current.duration;
+						const width = videoRef.current.videoWidth;
+						const height = videoRef.current.videoHeight;
+						setVideoMetadata({ duration, width, height });
+					};
+				}
+			} catch (error) {
+				console.error('Error checking for audio:', error);
+				setHasAudio(false);
+				alert('The Following Video doesnot have audio'); // Set to false if there's an error
+			}
 		}
 	};
 
@@ -63,26 +69,30 @@ const VPlayer = () => {
 					accept='video/*'
 					onChange={loadVideo}
 				/>
-
 				<span>Choose a file</span>
 			</label>
 
-			{/* <label for='file'>Choose a file</label> */}
-
 			<div className='video-container'>
-				<canvas ref={canvasRef} width={650} height={460}></canvas>
-
-				<div className='video-wrapper'>
-					<video ref={videoRef} controls={false} onClick={togglePlay} />
-				</div>
+				{hasAudio === false ? (
+					<canvas ref={canvasRef} width={650} height={460}>
+						<p className='error-message'>This video has no audio.</p>
+					</canvas>
+				) : (
+					<>
+						<canvas ref={canvasRef} width={650} height={460}></canvas>
+						<div className='video-wrapper'>
+							<video ref={videoRef} controls={false} onClick={togglePlay} />
+						</div>
+					</>
+				)}
 			</div>
 
 			<div className='video-info'>
 				<p>Duration: {videoMetadata.duration.toFixed(2)} seconds</p>
-
 				<p>
 					Dimensions: {videoMetadata.width} x {videoMetadata.height}
 				</p>
+				{hasAudio !== null && hasAudio !== false && <p>Has Audio: Yes</p>}
 			</div>
 		</div>
 	);
