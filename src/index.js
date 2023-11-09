d3c81ce4a7a7b37434f4c5624c622c7b7c596ded
vishant007 <vishant9797@gmail.com>
2023-11-09 16:47:38 +0530

    updated_indexJS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/App.js';
 import './index.css';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
