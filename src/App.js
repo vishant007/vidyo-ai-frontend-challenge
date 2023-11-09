113df099d28847b843f90914e93a7b20d05b96c2
vishant007 <vishant9797@gmail.com>
2023-11-09 17:00:34 +0530

    imports_updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Home from './components/Home';
+import Home from './components/Home.js';
 function App() {
 	return (
 		<div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import VPlayer from './VPlayer';
+import VPlayer from './VPlayer.js';
 import '../index.css';
 
 const Home = () => {
